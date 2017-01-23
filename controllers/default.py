b6718fef4e62fbc24036fef7b6f4d85e516e2097
James Iwamasa <jiwamasa@ucsc.edu>
2017-01-23 13:00:39 -0800

    Repurposed default 'layout.html' for our own use
    Site now has a navigation bar to go between the different pages.

--- controllers/default.py
+++ controllers/default.py
@@ -1,15 +1,23 @@
 #default controller
 
+#home page
 @auth.requires_login()
 def index():
+    return dict()
+
+#all quests page
+@auth.requires_login()
+def questsPage():
     questList = db().select(db.quests.ALL, orderby=db.quests.title)
     return dict(questList=questList)
 
+#details about a certain quest
 @auth.requires_login()
 def showQuest():
     quest = db.quests(request.args(0, cast=int)) or redirect(URL('index'))
     return dict(quest=quest)
 
+#quest adding page (shouldn't be public in final build)
 @auth.requires_login()
 def addQuest():
     grid = SQLFORM.smartgrid(db.quests)
--- models/db.py
+++ models/db.py
@@ -7,14 +7,6 @@ db.define_table('quests',
                 Field('reward'),
                 Field('details', 'text'))
 
-
-# Table containing adventurers for hire
-# db.define_table('adventurers',
-#                 Field('name', unique=True),
-#                 Field('hit points', 'int'),
-#                 Field('attack', 'int'),
-#                 format = '%(name)s')
-
 from gluon.tools import Auth
 auth = Auth(db)
 auth.define_tables(username=True)
