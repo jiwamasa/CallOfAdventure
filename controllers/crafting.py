d2ca9d4753bf62c46e29dd9dca98309339445702
James Iwamasa <jiwamasa@ucsc.edu>
2017-02-23 16:01:32 -0800

    When you forge, we choose name/description AFTER creation

--- controllers/crafting.py
+++ controllers/crafting.py
@@ -11,8 +11,7 @@ import forge_helper
 #forge equip items with rare ore
 def forge():
     current_user = db.auth_user(auth.user.id)
-    form = FORM('Name: ', INPUT(_name='itemname', _type='text', requires=IS_NOT_EMPTY()), BR(),
-                'Rare Ore: ', INPUT(_name='rareore', _type='text', requires=IS_INT_IN_RANGE(1, current_user.rare_ore+1, error_message='Enter valid amount of Rare Ore')), BR(),
+    form = FORM('Rare Ore: ', INPUT(_name='rareore', _type='text', requires=IS_INT_IN_RANGE(1, current_user.rare_ore+1, error_message='Enter valid amount of Rare Ore')), BR(),
                 'Booze: ', INPUT(_name='booze', _type='range'), BR(),
                 'Words of Encouragement: ', INPUT(_name='words', _type='text', requires=IS_NOT_EMPTY(error_message='Let him know you care!')),
                 BR(), INPUT(_type='submit', _value='FORGE!'))
@@ -23,13 +22,13 @@ def forge():
         new_rare_ore = current_user.rare_ore - rareore
         current_user.update_record(rare_ore=new_rare_ore)
         stats = forge_helper.calcStats(rareore,booze,form.vars.words)
-        forgeItem = db.equip_items.insert(name=form.vars.itemname,
+        forgeItem = db.equip_items.insert(name="Something",
                                           attack=stats[1],
                                           defense=stats[2],
                                           speed=stats[3],
                                           cost=100,
                                           category=stats[0],
-                                          details='test')
+                                          details="It's an item")
         current_inv = current_user.inventory
         current_inv.append(forgeItem)
         current_user.update_record(inventory=current_inv)
@@ -38,4 +37,7 @@ def forge():
 
 #results of forging
 def forgeResults():
-    return dict(forge_id=request.args(0))
+    form = SQLFORM(db.equip_items, record=request.args(0), fields=['name','details'], showid=False)
+    if form.process().accepted:
+        redirect(URL('default', 'index'))
+    return dict(forge_id=request.args(0),form=form)
