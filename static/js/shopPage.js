13cf7bd2c3295832fb5a49d3e0e3b8d58ad2c3f1
nbayt <nbayt@ucsc.edu>
2017-03-16 11:27:28 -0700

    Shop Page is actually fixed, hopefully...
    no more text overflow

--- static/js/shopPage.js
+++ static/js/shopPage.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function(){
   jQuery(".item").click(function(){
-    jQuery("#item_info").show();
+    jQuery("#buy_form").show();
     jQuery("#buy_form").attr("item_id",jQuery(this).attr("id"))
     ajax('/callofadventure/default/itemInfo/'+jQuery(this).attr("id"), [], ':eval');
   });
   jQuery("#buy_form").click(function(){
     ajax('/callofadventure/default/buyItem/'+jQuery(this).attr("item_id"), [], ':eval');
   });
-  jQuery("#item_info").hide();
+  jQuery("#buy_form").hide();
 });
