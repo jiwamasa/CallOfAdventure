e54097b64b6fded040cdd73550586d39b7fec457
James Iwamasa <jiwamasa@ucsc.edu>
2017-03-22 15:12:05 -0700

    So actually, there was a very stupid bug in the embark controller.
    I fixed it. Was actually just a typo. Sessions "shouldn't" affect game
    actually. What was happening was that there was a bug if the player had a
    party.

--- controllers/embark.py
+++ controllers/embark.py
@@ -18,21 +18,24 @@ import datetime
 def web2unity():
     #print "testing"
     #return "Cave%2%Bob%99%99%99" #TESTING
+    print "attempting to connect to session..."
     session.connect(request) #get the session of the unity game
     if not session:
         print "bad session"
         return "error: bad session"
     curr_user = db.auth_user(session.auth.user.id)
-    print curr_user.first_name
+    print curr_user.first_name + " has connected"
 
     if not session.curr_quest:
+        print "no quest"
         return "error: no active quest"
     quest = db.quests(session.curr_quest)
+    print "got quest"
     stats = quest.location+"%"+str(quest.difficulty)+"%"
     user_list = [session.auth.user.id] #list of people on quest
     if session.party:
         for hire_id in session.party:
-            user_list.append(hire_list)
+            user_list.append(hire_id)
     for hire_id in user_list:
         hire = db.auth_user(hire_id)
         attack = 0
